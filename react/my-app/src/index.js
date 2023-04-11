a47f645725353755ceefbe4d703e8514ad870e30
김성민 <fromlist5@gmail.com>
2023-04-11 17:52:50 +0900

    app

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import UseEffectPage from './UseEffectPage';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <UseEffectPage />
+  <App />
 );
