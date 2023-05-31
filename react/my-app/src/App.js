dfb954a679e9f035738e57f3733fa9b3d8c24147
Kimsungmin <fromlist5@gmail.com>
2023-05-31 21:04:18 +0900

    app

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,24 +1,17 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/movie">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
